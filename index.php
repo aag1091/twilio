dfe840afc35251cf8dc9aa6862a9a98965492107
Avinash Gosavi <aag1091@gmail.com>
2014-10-29 17:42:16 -0400

    Added all files from Twilio-test

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require "./vendor/twilio/sdk/Services/Twilio.php";
+// set your AccountSid and AuthToken from www.twilio.com/user/account
+$AccountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+$AuthToken = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY";
+$client = new Services_Twilio($AccountSid, $AuthToken);
+$message = $client->account->messages->create(array(
+"From" => "YYY-YYY-YYYY",
+"To" => "XXX-XXX-XXXX",
+"Body" => "Test message!",
+));
+// Display a confirmation message on the screen
+echo "Sent message {$message->sid}";
